f68ebf655a5647089f733add18ceb55e47816f99
hu li <892995826@qq.com>
2020-02-27 09:14:35 +0800

    完成UI1.0

---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Class()//夹具类表
+        {
+            return View();
+        }
+
+        public ActionResult Inout()//出入库管理
+        {
+            return View();
+        }
+
+        public ActionResult Purchase()//采购及报修
+        {
+            return View();
+        }
+
+        public ActionResult Handle()//处理
+        {
+            return View();
+        }
+
+        public ActionResult Verify()//审核
+        {
+            return View();
+        }
+        public ActionResult Data()//预警实时数据
+        {
+            return View();
+        }
+        public ActionResult Analysis()//分析
+        {
+            return View();
+        }
+        public ActionResult Person()//个人
+        {
+            return View();
+        }
+        public ActionResult Admin()//管理
+        {
+            return View();
+        }
+    }
+}
---
+++ UI/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult submit(string username, string userpassword)
+        {
+            //引入数据库
+            if (username == "admin" && userpassword == "123")
+            {
+                return Json(new
+                {
+                    Success = true,
+                });
+
+            }
+             else
+            {
+                    return Json(new
+                    {
+                        Success = false,
+                        Message = "当前用户不存在"
+                    });
+             }
+
+        }
+
+            
+    }
+}
